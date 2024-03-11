ec4417acdb30f7e9f84625591cdd41baa17dff2c
Deadlymind <deadlygamex5@gmail.com>
2024-03-11 14:09:18 +0100

    Add custom User model with CustomUserManager, extending AbstractUser and PermissionsMixin

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 # Create your models here.
+
+class User(AbstractUser, PermissionsMixin):
+
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+

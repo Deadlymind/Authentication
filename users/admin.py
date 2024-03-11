11cee5a4c5a134e0d8c04d7d5009e299a09b252b
Deadlymind <deadlygamex5@gmail.com>
2024-03-11 14:54:37 +0100

    Configure admin fieldsets and add_fieldsets for custom User model

--- users/admin.py
+++ users/admin.py
@@ -16,5 +16,37 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ['is_staff', 'is_superuser', 'is_active']
     search_fields = ['first_name', 'last_name', 'email']
 
+    fieldsets = (
+        (
+            _("Login Credentials"), {
+                "fields": ("email", "password",)
+            }, 
+        ),
+        (
+            _("Personal Information"),
+            {
+                "fields": ('first_name', 'last_name',)
+            },
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+            },
+        ),
+        (
+            _("Important Dates"),
+            {
+                "fields": ("last_login",)
+            },
+        ),
+    )
+    add_fieldsets = (
+            (None, {
+                "classes": ("wide",),
+                "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+            },),
+        )
+
 
 admin.site.register(User, UserAdmin)

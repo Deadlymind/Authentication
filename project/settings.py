dd96ca9f9fbf13a120f13be7f24b6dcd4c5d52d1
Deadlymind <deadlygamex5@gmail.com>
2024-03-11 17:08:15 +0100

    add rest_framework_auth in settings

--- project/settings.py
+++ project/settings.py
@@ -143,6 +143,5 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        (...)
     ),
 }

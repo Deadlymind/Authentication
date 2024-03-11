1cd7819644c31c96bed762807f28bf6e27634421
Deadlymind <deadlygamex5@gmail.com>
2024-03-11 17:07:56 +0100

    add rest_framework_auth in settings

--- project/settings.py
+++ project/settings.py
@@ -139,3 +139,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        (...)
+    ),
+}

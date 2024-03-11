a05c100737db4acd35bae89b9dd877f8f2b013ce
Deadlymind <deadlygamex5@gmail.com>
2024-03-11 17:05:06 +0100

    update requirements

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 # Register your models here.
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ['email']
+    ordering = ["email"]
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email', 'first_name', 'last_name', 'is_staff']
-    list_display_links = ['email']
-    list_filter = ['is_staff', 'is_superuser', 'is_active']
-    search_fields = ['first_name', 'last_name', 'email']
-
+    list_display = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    list_display_links = ["email"]
+    list_filter = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    search_fields = ["email", "first_name", "last_name"]
     fieldsets = (
         (
             _("Login Credentials"), {
--- users/models.py
+++ users/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 
 # Create your models here.
 
-class User(AbstractUser, PermissionsMixin):
 
-    first_name = models.CharField(_("first name"), max_length=150)
-    last_name = models.CharField(_("last name"), max_length=150)
-    email = models.EmailField(_("email address"), unique=True)
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("First Name"), max_length=100)
+    last_name = models.CharField(_("Last Name"), max_length=100)
+    email = models.EmailField(_("Email Address"), max_length=254, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    date_joined =  models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
@@ -20,8 +20,8 @@ class User(AbstractUser, PermissionsMixin):
     objects = CustomUserManager()
 
     class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
 
     def __str__(self):
         return self.email
@@ -29,6 +29,3 @@ class User(AbstractUser, PermissionsMixin):
     @property
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
-
-
-
